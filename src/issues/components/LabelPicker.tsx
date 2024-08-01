e798e6fc8b074d52c7a1fc1fbef3efcf02c83961
Lorena Rojas <loreprocks@Lorenas-MacBook-Pro.local>
2024-07-31 21:19:36 -0500

    following section 3 of the course

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,12 +1,33 @@
+import { LoadingSpinner } from "../../shared";
+import useLabels from "../hooks/useLabels";
+
+
+
 export const LabelPicker = () => {
+  
+ const { labelsQuery } = useLabels();
+
+
+  if(labelsQuery.isLoading) {
+    return (
+      <div className="flex justify-center items-center h-52"> 
+        <LoadingSpinner/>
+      </div>
+    )
+  }
+
+
   return (
-    <>
+    <div className="flex flex-wrap gap-2 justify-center">
+    {labelsQuery.data?.map((label) => (
       <span
-        className="px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer"
-        style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
+        className="animate-fadeIn px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer text-white"
+        style={{ border: `1px solid #${label.color}` }}
+        key={label.id}
       >
-        Primary
+        {label.name}
       </span>
-    </>
+    ))}
+    </div>
   );
 };
