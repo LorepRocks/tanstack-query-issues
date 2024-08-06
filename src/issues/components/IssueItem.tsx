a3ec5e6f8fcd5df69927351d1064737c3757b84c
Lorena Rojas <loreprocks@Lorenas-MacBook-Pro.local>
2024-08-06 16:52:38 -0500

    adding prefetch functions on mouseEnter and using the setQueryData funtion as well

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -1,6 +1,8 @@
 import { FiInfo, FiMessageSquare, FiCheckCircle } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
 import { GithubIssue, State } from "../interfaces/issue.interface";
+import { useQueryClient } from "@tanstack/react-query";
+import { getIssue, getIssueComments } from "../actions";
 
 interface Props {
   issue: GithubIssue;
@@ -8,9 +10,45 @@ interface Props {
 
 export const IssueItem = ({ issue }: Props) => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  /*
+  This makes a pre-fetch of the data on mouseenter event 
+  this actually makes the http request and keep the data 
+  on fresh for 1 hour
+  */
+  const preFetchData = () => {
+    console.log("___prefetching");
+    queryClient.prefetchQuery({
+      queryKey: ["issues", issue.number],
+      queryFn: () => getIssue(issue.number),
+      staleTime: 60000,
+    });
+    queryClient.prefetchQuery({
+      queryKey: ["issues", issue.number, "comments"],
+      queryFn: () => getIssueComments(issue.number),
+      staleTime: 6000,
+    });
+  };
+
+  /*
+    This set the data for a queryQuey in this case we are sending the issue
+    number to that specific query key and we aren't making the http request
+    to avoid make the request we need to use the updateAt parameter 
+    so in this case we'll keep the data fresh for 1 minute 
+   */
+  const presetData = () => {
+    queryClient.setQueryData(["issues", issue.number], issue, {
+      updatedAt: Date.now() + 1000 * 60,
+    });
+  };
 
   return (
-    <div className="animate-fadeIn flex items-center px-2 py-3 mb-5 border rounded-md bg-slate-900 hover:bg-slate-800">
+    <div
+      /* onMouseEnter={preFetchData} */
+      onMouseEnter={presetData}
+      className="animate-fadeIn flex items-center px-2 py-3 mb-5 border rounded-md bg-slate-900 hover:bg-slate-800"
+    >
       {issue.state === State.Close ? (
         <FiCheckCircle size={30} color="green" className="min-w-10" />
       ) : (
